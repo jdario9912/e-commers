eaa865211a4391eb4a195d6821335ba0a5436958
Joel Dario <joeldario9912@gmail.com>
2022-04-12 21:04:33 -0300

    resolvi el formulario de busqueda

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,20 @@
-const lupa = document.querySelector('[data-lupa]');
-const divBuscar = document.querySelector('[data-buscar]')
-const inputBuscar = document.querySelector('[data-input-buscar]');
+// const lupa = document.querySelector('[data-lupa]');
+// const divBuscar = document.querySelector('[data-buscar]')
+// const inputBuscar = document.querySelector('[data-input-buscar]');
 
-lupa.addEventListener('click', () =>{
-    // inputBuscar.hidden = true ? inputBuscar.hidden = false : null;
-    lupa.classList.toggle('display__none');
-    divBuscar.classList.toggle('buscar');
-    inputBuscar.classList.toggle('display__none');
-    inputBuscar.classList.toggle('buscar-input');
-    inputBuscar.value = '';
-    inputBuscar.focus();
-});
+// lupa.addEventListener('click', () =>{
+//     // inputBuscar.hidden = true ? inputBuscar.hidden = false : null;
+//     lupa.classList.toggle('display__none');
+//     divBuscar.classList.toggle('buscar');
+//     inputBuscar.classList.toggle('display__none');
+//     inputBuscar.classList.toggle('buscar-input');
+//     inputBuscar.value = '';
+//     inputBuscar.focus();
+// });
 
-inputBuscar.addEventListener('blur', () => {
-    lupa.classList.toggle('display__none');
-    divBuscar.classList.toggle('buscar');
-    inputBuscar.classList.toggle('display__none');
-    inputBuscar.classList.toggle('buscar-input');
-});
+// inputBuscar.addEventListener('blur', () => {
+//     lupa.classList.toggle('display__none');
+//     divBuscar.classList.toggle('buscar');
+//     inputBuscar.classList.toggle('display__none');
+//     inputBuscar.classList.toggle('buscar-input');
+// });
