4d9b7a48b37ac91cef9a4e97aeff2a60ed4fef09
Joel Dario <joeldario9912@gmail.com>
2022-04-21 22:52:40 -0300

    agregue el header y el footer como componentes al index y a login

---
+++ assets/js/components/footer.js
@@ -0,0 +1,47 @@
+class Footer extends HTMLElement {
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+        this.innerHTML= `
+            <footer class="footer">
+                <div class="footer__nosotros">
+                    <div class="logo footer-logo">
+                        <div class="logo-imagen"></div>
+                        <h1 class="logo-titulo">
+                            <em class="logo-alura">alura </em>geek
+                        </h1>
+                    </div>
+                    <nav class="footer__nav">
+                        <ul class="footer__lista">
+                            <li class="footer__item"><a href="#" class="footer__link">Quienes somos</a></li>
+                            <li class="footer__item"><a href="#" class="footer__link">Política de privacidad</a></li>
+                            <li class="footer__item"><a href="#" class="footer__link">Programa de fidelidad</a></li>
+                            <li class="footer__item"><a href="#" class="footer__link">Nuestras tiendas</a></li>
+                            <li class="footer__item"><a href="#" class="footer__link">Quiero una franquicia</a></li>
+                            <li class="footer__item"><a href="#" class="footer__link">Anúncia aquí</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <form action="" class="footer__formulario">
+                        <p class="footer__parrafo">Hable con nosotros</p>
+                        <label class="footer__label-nombre" for="nombre">
+                            Nombre:
+                            <input type="text" class="footer__input-nombre" id="nombre" name="nombre">
+                        </label>
+                        <label class="footer__label-mensaje" for="mensaje">
+                            Mensaje:
+                            <textarea name="mensaje" id="mensaje" cols="30" rows="10" class="footer__mensaje"></textarea>
+                        </label>
+                        <button class="footer__btn">Enviar mensaje</button>
+                    </form>
+            </footer>
+            <section class="rodapie">
+                <p class="rodapie__parrafo">&#169 Copyrigth <time>2022</time> | Creado por <em class="rodapie__nombre">Joel</em></p>
+            </section>
+        `;
+    }
+}
+
+customElements.define('footer-component', Footer);
---
+++ assets/js/components/header.js
@@ -0,0 +1,26 @@
+class Header extends HTMLElement {
+    constructor(){
+        super();
+    }
+
+    connectedCallback(){
+        this.innerHTML= `
+            <header class="header">
+                <div class="logo header-logo">
+                    <div class="logo-imagen"></div>
+                    <h1 class="logo-titulo">
+                        <em class="logo-alura">alura </em>geek
+                    </h1>
+                </div>
+                <a class="header__login" href="#" target="_blank">Login</a>
+                <form class="header__formulario" action="">
+                    <input class="header__input" type="text" name="buscar" id="buscar" hidden>
+                    <button class="header__btn" hidden></button>
+                </form>
+                <div class="header__lupa"></div>
+            </header>
+        `;
+    }
+}
+
+customElements.define('header-component', Header);
