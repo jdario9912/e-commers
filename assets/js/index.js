3326333c702bd69c1378ca82274a436f340c457c
Joel Dario <joeldario9912@gmail.com>
2022-04-30 01:38:35 -0300

    agregue responsividad para todos los dispositivos a la pagina de agregar productos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,33 @@
-const lupa = document.querySelector('[data-lupa]');
-const divBuscar = document.querySelector('[data-buscar]')
-const inputBuscar = document.querySelector('[data-input-buscar]');
+// const lupa = document.querySelector('[data-lupa]');
+// const divBuscar = document.querySelector('[data-buscar]')
+// const inputBuscar = document.querySelector('[data-input-buscar]');
 
-lupa.addEventListener('click', () =>{
-    // inputBuscar.hidden = true ? inputBuscar.hidden = false : null;
-    lupa.classList.toggle('display__none');
-    divBuscar.classList.toggle('buscar');
-    inputBuscar.classList.toggle('display__none');
-    inputBuscar.classList.toggle('buscar-input');
-    inputBuscar.value = '';
-    inputBuscar.focus();
-});
+// lupa.addEventListener('click', () =>{
+//     // inputBuscar.hidden = true ? inputBuscar.hidden = false : null;
+//     lupa.classList.toggle('display__none');
+//     divBuscar.classList.toggle('buscar');
+//     inputBuscar.classList.toggle('display__none');
+//     inputBuscar.classList.toggle('buscar-input');
+//     inputBuscar.value = '';
+//     inputBuscar.focus();
+// });
 
-inputBuscar.addEventListener('blur', () => {
-    lupa.classList.toggle('display__none');
-    divBuscar.classList.toggle('buscar');
-    inputBuscar.classList.toggle('display__none');
-    inputBuscar.classList.toggle('buscar-input');
-});
+// inputBuscar.addEventListener('blur', () => {
+//     lupa.classList.toggle('display__none');
+//     divBuscar.classList.toggle('buscar');
+//     inputBuscar.classList.toggle('display__none');
+//     inputBuscar.classList.toggle('buscar-input');
+// });
 
 
-window.addEventListener('resize', () => {
-    const orientacionPantalla = window.screen.orientation;
-    console.log(orientacionPantalla);
+// window.addEventListener('resize', () => {
+//     const orientacionPantalla = window.screen.orientation;
+//     console.log(orientacionPantalla);
     
-});
+// });
 
-window.addEventListener('orientationchange', () => {
-    const orientacionPantalla = window.screen.orientation.angle;
-    console.log(orientacionPantalla);
+// window.addEventListener('orientationchange', () => {
+//     const orientacionPantalla = window.screen.orientation.angle;
+//     console.log(orientacionPantalla);
     
-});
+// });
